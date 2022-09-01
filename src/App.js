9389237b837cc0f8ffea6cb1f4ed3bc8f78846ae
Ashutosh rai <67899702+ashurajchandra@users.noreply.github.com>
2022-09-01 14:47:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Comment box</h1>
+     <div>
+       <h5>Ashutosh Rai</h5>
+       <p>first comment!!!!!</p>
+     </div>
+    </div>
+  );
+}
